8f6ae6fafb478abb2a3926e8a2c58c9707479921
abel abadi <abelabadi2@gmail.com>
2016-05-17 11:36:29 -0500

    ajax request & json response
    added jsonsimple labrary. make request from client to servlet and got
    json response.

--- src/java/servlets/dictServlet.java
+++ src/java/servlets/dictServlet.java
@@ -11,38 +11,14 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
+import org.json.simple.JSONObject;
 /**
  *
  * @author abelo
  */
 public class dictServlet extends HttpServlet {
 
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet dictServlet</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet dictServlet at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-        }
-    }
+   
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
     /**
@@ -56,7 +32,6 @@ public class dictServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
     }
 
     /**
@@ -70,7 +45,11 @@ public class dictServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+ 
+        PrintWriter out = response.getWriter();
+        JSONObject obj=new JSONObject();
+        obj.put("word","definition from server");
+        out.print(obj); out.flush();
     }
 
     /**
--- web/js/dict.js
+++ web/js/dict.js
@@ -1,1 +1,23 @@
 // JavaScript event handlers for dict.html 
+$(function () {
+	Dictionary.init();
+});
+
+var Dictionary = {
+	word : $('#input-word').val(),
+	init: function() {
+            $('#submit-word').on('click',function () {
+                    Dictionary.sendAjaxRequest();
+            });
+	},
+	sendAjaxRequest: function () {
+		$.ajax({
+                    url:'dictServlet',
+                    type:'post',
+                    dataType:'json',
+                    success:function (response) {
+                    	console.log(response);
+                    }
+		});
+	}
+};
