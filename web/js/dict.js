56a391e9bcabea778d76968cf0dd24134129a0c1
abel abadi <abelabadi2@gmail.com>
2016-05-18 02:00:03 -0500

    fixed bugg appending content to previous
    clear element before appending new result.

--- web/js/dict.js
+++ web/js/dict.js
@@ -25,7 +25,8 @@ var Dictionary = {
 	},
         displayResponse: function (response) {
             //console.log(response);
-            //console.log(response[key].definition)  
+            //console.log(response[key].definition)
+            $(".col-lg-6 ol").html("");
             $("row .col-lg-6").append('<b>'+response[0].word+'</b>');
             for (var key in response) {
                 $(".col-lg-6 ol").append('<li><b>'+response[key].wordtype+'</b>'+" "+response[key].definition+'</li>');
