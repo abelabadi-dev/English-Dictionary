485717181d56a5387eac9ea7c6fae16e054749b6
abel abadi <abelabadi2@gmail.com>
2016-05-18 01:49:24 -0500

    append result to the dom
    display definition to browser.

--- web/js/dict.js
+++ web/js/dict.js
@@ -24,7 +24,13 @@ var Dictionary = {
             });
 	},
         displayResponse: function (response) {
-            console.log(response);
+            //console.log(response);
+            //console.log(response[key].definition)  
+            $("row .col-lg-6").append('<b>'+response[0].word+'</b>');
+            for (var key in response) {
+                $(".col-lg-6 ol").append('<li><b>'+response[key].wordtype+'</b>'+" "+response[key].definition+'</li>');
+            }
+
 //            $.each(function (index,value) {
 //                console.log(index);
 //                console.log(value);
